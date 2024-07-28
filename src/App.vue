4e54700046e1d75ae727b8e5c15b90495fa4baa2
Ansanat <Ansanat228@gmail.com>
2024-07-28 13:15:18 +0700

    ask

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <AboutMe/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AboutMe from './components/about-me.vue'
+import '@/icons/style.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AboutMe
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+$background: #191919;
+
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: $background;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/about-me.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="about-me">
+    <div class="about-me-name"> {{name}} </div>
+    <div class="about-me-spec"> {{spec}} </div>
+    <div class="about-me-line"></div>
+    <div class="about-me-info"> {{info}} </div>
+    <div 
+      class="about-me-about-me"
+      v-for="item in aboutMeList" :key="item"
+    >
+      <i class="about-me-about-me-icon"></i>
+      <div class="about-me-about-me-item"></div>
+    </div>
+    <div class="about-me-line"></div>
+  </div>
+</template>
+
+<script>
+import '@/icons/style.css'
+
+export default {
+  name: 'AboutMe',
+  props: {
+  },
+  data() {
+    return {
+      name: 'Виталий Колточихин',
+      spec: 'Web-разработчик',
+      info: 'Доброго времени суток! Меня зовут Виталий. Я занимаюсь профессиональной разработкой сайтов под ключ. О себе:',
+      aboutMeList: [
+        {
+          icon: '',
+          text: 'Действующий web-разработчик в аккредитованой IT-компании'
+        },
+        {
+          icon: '',
+          text: 'Большой опыт создания и ведения сайтов для бизнеса'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.about-me {
+  color: white;
+  padding: 40px;
+
+  &-name {
+    font-size: 30px;
+  }
+
+  &-spec {
+    font-size: 20px;
+    opacity: 0.7;
+  }
+
+  &-line {
+    width: 100%;
+    height: 1px;
+    background-color: white;
+    margin-top: 10px;
+  }
+
+  &-info {
+    margin-top: 10px;
+    text-align: justify;
+    font-size: 16px;
+    opacity: 0.9;
+  }
+}
+</style>
